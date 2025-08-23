5d522120e2562d66226174229f7b16d6a7688579
MarcoAntonio-lab <MarcoAntonio-lab@gmail.com>
2025-08-22 21:12:56 -0500

    Tercer commit

---
+++ src/ejer2/Ejer2.java
@@ -0,0 +1,29 @@
+
+package ejer2;
+
+
+public class Ejer2 {
+     public static void burbuja(int [] arr){
+        int n = arr.length; 
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+       // ordenacion por burbuja 
+    int [] numeros = {4, 5, 6, 7, 8}; 
+    burbuja(numeros); 
+    for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+
+}
+
+    
+}
