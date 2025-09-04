145a3658455d63792e38038021a314b222da4970
MarcoAntonio-lab <marcopaucarchoquehuanca15@gmail.com>
2025-09-03 22:52:15 -0500

    treceavo commit

--- src/AlgoritmoMezclaDirecta/DirectMergeSort.java
+++ src/AlgoritmoMezclaDirecta/DirectMergeSort.java
@@ -4,8 +4,31 @@ package AlgoritmoMezclaDirecta;
 
 public class DirectMergeSort {
     
+    public static void mezclaDirecta(int[] arr, int inicio, int fin) {
+        if (inicio < fin) {
+            int medio = (inicio + fin) / 2;
+
+            // Ordenar la primera mitad
+            mezclaDirecta(arr, inicio, medio);
+
+            // Ordenar la segunda mitad
+            mezclaDirecta(arr, medio + 1, fin);
+
+            // Mezclar ambas mitades
+            mezclar(arr, inicio, medio, fin);
+        }
+     }
     public static void main(String[] args) {
         
-        System.out.println("Probando por mientras");
+       int[] numeros = {8, 3, 1, 9, 5, 4, 2, 7, 6};
+
+        System.out.println("Arreglo original:");
+        mostrarArreglo(numeros);
+
+        mezclaDirecta(numeros, 0, numeros.length - 1);
+
+        System.out.println("\nArreglo ordenado:");
+        mostrarArreglo(numeros);
+    }
     }
 }
